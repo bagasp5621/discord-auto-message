4cd6ca33107c72c20c1a33de2a982e2e869481be
Bagas Prasetya <bagasp5621@gmail.com>
2024-03-25 19:20:26 +0700

    changes

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ platform_content = getContent("PLATFORM_CONTENT")
 header_data = { 
   "content-type": "application/json", 
   "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36", 
-  "authorization": "MzMzOTA4ODIzODk3MDE0Mjc1.G9MATa.Q8byB3jP-PSrsBdz-3jGHVzkYCBSLl9vG_D898", 
+  "authorization": "", 
   "host": "discordapp.com", 
   "referer": "https://discord.gg/thelostnemo" 
 } 
